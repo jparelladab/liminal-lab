8c04527fca9b0f5ddd18c279180fa59ae48fbb6b
joan parellada <joan.parellada789@gmail.com>
2021-03-11 00:48:38 +0100

    fixes sobre mi

--- page-sobre-mi.php
+++ page-sobre-mi.php
@@ -3,7 +3,7 @@
 ?>
 <div class="page-sobre-mi pos-rel" >
     <div id="sobre-mi"></div>
-	<div class="text1 pt-4 pb-5 x-padding pos-rel">
+	<div class="text1 pt-4 pb-4 x-padding pos-rel">
   	<div class="text-m" data-aos="fade-up" >
   		<div><span class="section-dot dot-para-que"></span><?php the_field('text1', 11) ?></div>
   	</div>
@@ -11,12 +11,12 @@
     <div class="border-l-abs"></div>
   </div>
   <div class="three-columns pos-rel">
-    <div class="text2 pt-4 pb-5 x-padding pos-rel">
+    <div class="text2 pt-4 pb-4 x-padding pos-rel">
       <div class="text-s" data-aos="fade-up"><?php the_field('text2', 11); ?></div>
-
+      <div class="border-b-abs"></div>
       <div class="border-l-abs"></div>
     </div>
-    <div class="text3 pt-4 pb-5 x-padding pos-rel">
+    <div class="text3 pt-4 pb-4 x-padding pos-rel">
       <div class="text-s" data-aos="fade-up"><?php the_field('text3', 11); ?></div>
       <div class="border-l-abs"></div>
     </div>
